9964d6b07f6a61d7450d00ce75916fd5e596695b
Zaya <zarina.kost11@gmail.com>
2017-10-01 22:06:19 +0600

    Zaika.java аздап озгерди

--- srs/Zaikaa/Zaika.java
+++ srs/Zaikaa/Zaika.java
@@ -6,5 +6,10 @@ public class Zaika {
         System.out.println("Бул зертханалык жумыстын максаты");
         System.out.println("GitHub-та жумыс жасап уйрену");
 
+        int a=MyMath.kvadrat(a:5);
+        int b=MyMath.kub(a:5);
+
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
     }
 }
