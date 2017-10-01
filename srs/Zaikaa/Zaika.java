bf5de36f2f3a8833b8b66bcaf8ddc38f3ff4d462
Zaya <zarina.kost11@gmail.com>
2017-10-01 21:29:51 +0600

    Екинши коммит

--- srs/Zaikaa/Zaika.java
+++ srs/Zaikaa/Zaika.java
@@ -3,5 +3,8 @@ package Zaikaa;
 public class Zaika {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Бул зертханалык жумыстын максаты");
+        System.out.println("GitHub-та жумыс жасап уйрену");
+
     }
 }
